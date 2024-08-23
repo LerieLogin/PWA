9b66ee6359f8fbbf7729e50aec436ef55f6db407
LerieLogin <161064384+LerieLogin@users.noreply.github.com>
2024-08-23 16:42:51 -0600

    finally got things fixed i think

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,5 +1,4 @@
 const butInstall = document.getElementById('buttonInstall');
-<<<<<<< HEAD
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (event) => {
@@ -20,15 +19,3 @@ window.addEventListener('appinstalled', () => {
   deferredPrompt = null;
   butInstall.style.display = 'none';
 });
-=======
-
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
->>>>>>> 79f3e56d1233ca5665868c6c2831da93c2f9d8bb
